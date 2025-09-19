3636d29ba05cfabbe06f808ade6ab0ad68c5bdc8
promisetaiwoola29@gmail.com <promisetaiwoola29@gmail.com>
2025-09-19 16:49:28 +0100

    multiline quote

---
+++ c-introduction/quote.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    printf("The best way to predict the future is to invent it.\n");
+    printf("Knowledge is power.\n");
+    printf("Practice makes perfect.\n");
+    return 0;
+}
