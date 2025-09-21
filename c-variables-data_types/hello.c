9b82f0a7db3785be1dc7508b8aa9251e97ea9fe8
promisetaiwoola29@gmail.com <promisetaiwoola29@gmail.com>
2025-09-21 22:13:35 +0100

    Add hello.c with comments and Hello, C programming! output

---
+++ c-variables-data_types/hello.c
@@ -0,0 +1,15 @@
+// This program prints a greeting for the C variables/data types project
+/*
+C remains crucial because of its close-to-hardware control and efficiency, forming the base for many modern languages.
+*/
+#include <stdio.h>
+/**
+ * main - the main function
+ * description: Entry point for our variables/data types project
+ * return: 0
+*/
+int main(void)
+{
+    printf(\"Hello, C programming!\\n\");
+    return 0;
+}
