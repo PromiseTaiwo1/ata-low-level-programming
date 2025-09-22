48740c2c7ea1edd73b0d1c929987e046b274d0ce
promisetaiwoola29@gmail.com <promisetaiwoola29@gmail.com>
2025-09-22 11:17:54 +0100

    compilation_demo.c for compilation demo

---
+++ c-variables-data_types/compilation_demo.c
@@ -0,0 +1,14 @@
+/**
+ * main - the entry point of the program
+ * description: Prints a message about compilation in C.
+ * return: Always 0 (success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	    printf("Compilation in C works!\n");
+	    return 0;
+}
+
