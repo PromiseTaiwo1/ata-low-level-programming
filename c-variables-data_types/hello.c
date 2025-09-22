4c6b2ecf6723f25245b44cb6c8615884758ac25a
promisetaiwoola29@gmail.com <promisetaiwoola29@gmail.com>
2025-09-22 10:36:56 +0100

    main comment comes first

--- c-variables-data_types/hello.c
+++ c-variables-data_types/hello.c
@@ -1,15 +1,20 @@
-// This program prints a greeting for the C variables/data types project
-/*
-C remains crucial because of its close-to-hardware control and efficiency, forming the base for many modern languages.
-*/
-#include <stdio.h>
 /**
  * main - the main function
  * description: Entry point for our variables/data types project
- * return: 0
+ * return: 0     
+ */
+// This program prints a greeting for the C variables/data types project
+/*
+C remains crucial because of its close-to-hardware control and efficiency,
+forming the base for many modern languages.
 */
+
+#include <stdio.h>
+
 int main(void)
 {
-    printf(\"Hello, C programming!\\n\");
+    printf("Hello, C programming!\n");
     return 0;
 }
+
+
