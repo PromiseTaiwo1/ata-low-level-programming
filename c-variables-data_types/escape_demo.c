ba2750e58b84f11da3c3f17c811481d10f8b6f71
promisetaiwoola29@gmail.com <promisetaiwoola29@gmail.com>
2025-09-24 22:34:26 +0100

    escape sequence showcase

---
+++ c-variables-data_types/escape_demo.c
@@ -0,0 +1,14 @@
+/**
+ * main - the main function
+ * description: Demonstrates the use of escape sequences to print formatted text with new lines and tabs.
+ * return: 0
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Line1\n\tLine2\n");
+    printf("Tabbed\\tText\n");
+    return 0;
+}
+
