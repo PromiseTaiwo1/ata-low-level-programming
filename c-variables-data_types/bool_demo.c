9735b22553b79353f190f3d95e853dcf1c649963
promisetaiwoola29@gmail.com <promisetaiwoola29@gmail.com>
2025-09-24 22:47:06 +0100

    Bool in C

---
+++ c-variables-data_types/bool_demo.c
@@ -0,0 +1,16 @@
+/**
+ * main - the main function
+ * description: Demonstrates the use of a boolean variable and prints its value.
+ * return: 0
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+_Bool flag = 1;
+
+int main() {
+    printf("The value of flag is %d\n", flag);
+    return 0;
+}
+
