256452e8d007bcbf0637d5c3ad875a1f646778ec
promisetaiwoola29@gmail.com <promisetaiwoola29@gmail.com>
2025-09-24 22:07:12 +0100

    integer fun

---
+++ c-variables-data_types/int_demo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * min - integer variable
+ * description: Stores the minimum value, set to -100.
+*/
+int min = -100;
+
+/**
+ * max - integer variable
+ * description: Stores the maximum value, set to 100.
+*/
+int max = 100;
+
+/**
+ * main - the main function
+ * description: Prints the values of 'min' and 'max' integer variables.
+ * return: 0
+*/
+int main() {
+    printf("Min: %d\n", min);
+    printf("Max: %d\n", max);
+    return 0;
+}
+
