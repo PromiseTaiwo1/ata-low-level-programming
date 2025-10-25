390e3d86a1ba302eb4e9bb62ddcf185bda40bb6f
promisetaiwoola29@gmail.com <promisetaiwoola29@gmail.com>
2025-10-25 17:41:17 +0100

    Conditional Ternary Operator Example

---
+++ c-control_structures/ternary_operator_example.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    (num > 0) ? printf("%d is a positive number.\n", num)
+	      : printf("%d is a non-positive number.\n", num);
+
+    return 0;
+}
