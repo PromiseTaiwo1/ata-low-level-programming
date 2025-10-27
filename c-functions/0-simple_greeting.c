82fa2b64a3b8cffd3497b2ceb0ea9188b74f7d2b
promisetaiwoola29@gmail.com <promisetaiwoola29@gmail.com>
2025-10-27 18:56:27 +0100

    My First Function

---
+++ c-functions/0-simple_greeting.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Function prototype */
+void print_hello(void);
+
+/* Function definition */
+void print_hello(void)
+{
+    printf("Hello, C Functions!\n");
+}
+
+/* Main function */
+int main(void)
+{
+    print_hello();
+    return 0;
+}
