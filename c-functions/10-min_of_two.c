55766df961ef4858f5a7e61101d635dfb35e6cc6
promisetaiwoola29@gmail.com <promisetaiwoola29@gmail.com>
2025-10-27 20:45:03 +0100

    Find Minimum of Two Numbers

---
+++ c-functions/10-min_of_two.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* Function prototype */
+int find_min(int a, int b);
+
+/* Function definition */
+int find_min(int a, int b)
+{
+    if (a < b)
+	return a;
+    else
+	return b;
+}
+
+/* Main function */
+int main(void)
+{
+    int num1, num2, min;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    min = find_min(num1, num2);
+
+    printf("Minimum number is: %d\n", min);
+
+    return 0;
+}
