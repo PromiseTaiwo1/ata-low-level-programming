d218b7b80c236b174717e1b12f844d2d2a527818
promisetaiwoola29@gmail.com <promisetaiwoola29@gmail.com>
2025-10-28 12:09:16 +0100

    Swapping Integers (Pass-by-Reference)

---
+++ c-functions/16-swap_integers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/* Function Prototype */
+void swap_integers(int *a, int *b);
+
+/* Function Definition */
+void swap_integers(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/* Main Function */
+int main(void)
+{
+    int num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    printf("Before swap: num1 = %d, num2 = %d\n", num1, num2);
+
+    swap_integers(&num1, &num2);
+
+    printf("After swap: num1 = %d, num2 = %d\n", num1, num2);
+
+    return 0;
+}
